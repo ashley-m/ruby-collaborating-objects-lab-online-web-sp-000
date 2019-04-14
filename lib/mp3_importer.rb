0494aaa5f1c18cffa4d8b7e76707721b249472ae
Ashley Miller <asm2fe@virginia.edu>
2019-04-14 17:47:16 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,4 +10,12 @@ class MP3Importer
     x = Dir["#{@path}/*"]
     x.each  {|y| y.slice!(/.*\//)}
   end
+
+  def import
+    files.each { |y|
+      a = y.split(/-|[.]/)
+      s = Song.new(a[1].strip)
+      s.artist = a[0].strip
+    }
+  end
 end
