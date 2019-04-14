eed9134d460e68d446269409d7d94c060a19cb0a
Ashley Miller <asm2fe@virginia.edu>
2019-04-14 17:53:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   def self.new_by_filename(filename)
     a = filename.split(/-|[.]/)
     s = Song.new("#{a[1].strip}")
-    s.artist = a[0].strip
+    s.artist = Artist.new(a[0].strip)
     binding.pry
     return s
   end
